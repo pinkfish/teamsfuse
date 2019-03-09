e82b5778f2249234fe1b8a361d2c7f4de4d5d10c
David Bennett <pinkfishfrog@gmail.com>
2019-03-09 20:08:43 +0000

    Fix up node test

--- firebase/functions/test/index.test.js
+++ firebase/functions/test/index.test.js
@@ -5,23 +5,25 @@ const assert = chai.assert;
 // Sinon is a library used for mocking or verifying function calls in JavaScript.
 const sinon = require('sinon');
 
-
 // Require firebase-admin so we can stub out some of its methods.
 const admin = require('firebase-admin');
-
+// Require and initialize firebase-functions-test. Since we are not passing in any parameters, it will
+// be initialized in an "offline mode", which means we have to stub out all the methods that interact
+// with Firebase services.
 const test = require('firebase-functions-test')();
 
 describe('Cloud Functions', () => {
     let myFunctions, adminInitStub, myDb;
 
     before(() => {
+        console.log('before');
         // If index.js calls admin.initializeApp at the top of the file,
         // we need to stub it out before requiring index.js. This is because the
         // functions will be executed as a part of the require process.
         // Here we stub admin.initializeApp to be a dummy function that doesn't do anything.
         adminInitStub = sinon.stub(admin, 'initializeApp');
         // Now we can require index.js and save the exports inside a namespace called myFunctions.
-        myFunctions = require('../db/gameresultnotify.f');
+        myFunctions = require('../db/gameresultnotify.f.js');
     });
 
     after(() => {
