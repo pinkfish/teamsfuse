e871cc15c82ad9490d6056240e5b493216c0299f
David Bennett <pinkfishfrog@gmail.com>
2019-03-13 16:49:10 +0000

    Add in start/stop functionality

--- firebase/emulator-test.sh
+++ firebase/emulator-test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]; then
+    echo "firebase-emulator {start|stop}"
+    exit 1
+fi
+
 EMULATOR="cloud-firestore-emulator"
 EMULATOR_TARGET=$(find ~/.cache/firebase/emulators/ -type f -name "$EMULATOR*.jar" | sort -r | head -n1)
 
@@ -19,28 +24,42 @@ killEmulatorPid()
   fi
 }
 
-java -jar "$EMULATOR_TARGET" --host=127.0.0.1 --port=9090 > /dev/null 2> firestore-emulator.log &
-RETRIES=0
-RETRY_LIMIT=10
+startEmulator()
+{
+  java -jar "$EMULATOR_TARGET" --host=127.0.0.1 --port=9090 > /dev/null 2> firestore-emulator.log &
+  RETRIES=0
+  RETRY_LIMIT=10
 
-while [ $RETRIES -lt $RETRY_LIMIT ]; do
-  sleep 1
-  echo "Pinging firestore emulator"
+  while [ $RETRIES -lt $RETRY_LIMIT ]; do
+    sleep 1
+    echo "Pinging firestore emulator"
 
-  if nc -z localhost 9090; then
-    break
-  fi
+    if nc -z localhost 9090; then
+      break
+    fi
 
-  let RETRIES+=1
+    let RETRIES+=1
 
-  if [ $RETRIES -ge $RETRY_LIMIT ]; then
-    echo "Could not find the firestore emulator. Ending test run."
+    if [ $RETRIES -ge $RETRY_LIMIT ]; then
+      echo "Could not find the firestore emulator. Ending test run."
+      killEmulatorPid
+      exit 1
+    fi
+  done
+}
+
+case $1 in
+  start)
+    echo "Starting the emulator"
+    startEmulator
+    exit 0
+  stop)
+    echo "Stopping the emulator"
     killEmulatorPid
+    exit 0
+  *)
+    echo "firebase-emulator {start|stop}"
     exit 1
-  fi
-done
-
-npm test
+esac
 
 echo "End of test run. Cleaning up the firestore emulator."
-killEmulatorPid
