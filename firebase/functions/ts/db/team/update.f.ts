8d1daa5f773cad4bd6675aaf76f60638f82634da
David Bennett <pinkfishfrog@gmail.com>
2019-09-06 01:11:33 -0700

    Fix up the teams users set when it changes.

--- firebase/functions/ts/db/team/update.f.ts
+++ firebase/functions/ts/db/team/update.f.ts
@@ -1,5 +1,9 @@
 import * as functions from 'firebase-functions';
 import * as algolia from '../../util/algolia';
+import * as admin from 'firebase-admin';
+import { updateUsers } from './helper';
+
+const db = admin.firestore();
 
 // Handle the creation case as well, so if we create a game
 // with a specific result we update the team values.
@@ -10,6 +14,16 @@ export const onTeamUpdate = functions.firestore.document('/Team/{leagueId}').onU
     // See if the name changed.
     if (previousData!.name === null || previousData!.name !== data!.name) {
         return algolia.updateTeam(inputData.after);
+    }
+    if (data && data.users) {
+        const newData = updateUsers(data.users);
+        if (newData.size > 0) {
+            // Do the update.
+            return db
+                .collection('Team')
+                .doc(inputData.after.id)
+                .update(newData);
+        }
     }
     return data;
 });
