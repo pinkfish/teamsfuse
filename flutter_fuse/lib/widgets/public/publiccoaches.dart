5426fd635095dc4a10138afd9b997c51095581c0
David Bennett <pinkfishfrog@gmail.com>
2021-02-11 14:01:10 -0800

    Put in spaces between coaches.

--- flutter_fuse/lib/widgets/public/publiccoaches.dart
+++ flutter_fuse/lib/widgets/public/publiccoaches.dart
@@ -18,32 +18,35 @@ class PublicCoachDetails extends StatelessWidget {
   PublicCoachDetails(this.bloc);
 
   Widget _buildCoach(BuildContext context, Coach coach) {
-    return Card(
-      margin: EdgeInsets.all(5.0),
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Row(
-          children: [
-            CoachImage(
-                coachUid: coach.uid,
-                clubUid: coach.clubUid,
-                height: 200,
-                width: 200),
-            SizedBox(width: 10),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(coach.name,
-                      style: Theme.of(context).textTheme.headline5),
-                  SizedBox(height: 15),
-                  Text(coach.about,
-                      style: Theme.of(context).textTheme.bodyText1),
-                ],
+    return Padding(
+      padding: EdgeInsets.only(left: 0, right: 0, bottom: 20),
+      child: Card(
+        margin: EdgeInsets.all(5.0),
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              CoachImage(
+                  coachUid: coach.uid,
+                  clubUid: coach.clubUid,
+                  height: 200,
+                  width: 200),
+              SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(coach.name,
+                        style: Theme.of(context).textTheme.headline5),
+                    SizedBox(height: 15),
+                    Text(coach.about,
+                        style: Theme.of(context).textTheme.bodyText1),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -57,27 +60,33 @@ class PublicCoachDetails extends StatelessWidget {
             bloc.add(SingleClubLoadCoaches());
           }
           if (state is SingleClubUninitialized || !state.loadedCoaches) {
-            return Text(Messages.of(context).loading,
-              style: Theme.of(context).textTheme.headline4,);
+            return Text(
+              Messages.of(context).loading,
+              style: Theme.of(context).textTheme.headline4,
+            );
           }
           if (state is SingleClubDeleted) {
-            return Text(Messages.of(context).clubDeleted,
-              style: Theme.of(context).textTheme.headline4,);
+            return Text(
+              Messages.of(context).clubDeleted,
+              style: Theme.of(context).textTheme.headline4,
+            );
           }
           return Column(
             children: [
               SizedBox(height: 10),
-              Row(children: [
-                ClubImage(
-                  clubUid: state.club.uid,
-                  width: 100,
-                  height: 100,
-                ),
-                Text(
-                state.club.name,
-                style: Theme.of(context).textTheme.headline4,
+              Row(
+                children: [
+                  ClubImage(
+                    clubUid: state.club.uid,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Text(
+                    state.club.name,
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
               ),
-              ],),
               SizedBox(height: 10),
               Text(
                 Messages.of(context).coaches,
