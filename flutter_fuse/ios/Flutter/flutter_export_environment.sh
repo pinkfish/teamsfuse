747594fe69f154ed513ab73bf4ae6e4aefa9b638
GitHub Action <action@github.com>
2021-02-11 18:49:34 +0000

    Release notes for the ios 0.9.2 version

--- flutter_fuse/ios/Flutter/flutter_export_environment.sh
+++ flutter_fuse/ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/hostedtoolcache/flutter/1.26.0-17.4.pre-beta/x64"
+export "FLUTTER_ROOT=/opt/hostedtoolcache/flutter/1.26.0-17.5.pre-beta/x64"
 export "FLUTTER_APPLICATION_PATH=/home/runner/work/teamsfuse/teamsfuse/flutter_fuse"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
