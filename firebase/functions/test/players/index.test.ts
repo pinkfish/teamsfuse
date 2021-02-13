22572f0b2709f3a4c54503872543d0620b86e267
David Bennett <pinkfishfrog@gmail.com>
2021-02-13 11:07:40 -0800

    Fix up the rmeove user flow.

--- firebase/functions/test/players/index.test.ts
+++ firebase/functions/test/players/index.test.ts
@@ -59,7 +59,7 @@ describe('Player Tests', () => {
         }
     });
 
-    it('update with a user, in a team', async () => {
+    it('update add user, in a team', async () => {
         // Just make sure creating a player actually works.
         const player = await createPlayer(['me', 'other'], 'player');
         const teamAndSeason = await createSeasonAndTeam(false, false);
@@ -104,13 +104,15 @@ describe('Player Tests', () => {
                         player: true,
                     },
                     other: {
-                       added: true,
-                       player: true,
-                    }
+                        added: true,
+                        player: true,
+                    },
                 });
                 expect(myData.players).to.deep.equal({
                     player: {
-                         added: true, me: true, other: true,
+                        added: true,
+                        me: true,
+                        other: true,
                     },
                 });
             }
@@ -130,6 +132,90 @@ describe('Player Tests', () => {
                         added: true,
                         player: true,
                     },
+                });
+            }
+        }
+    });
+
+    it('update remove user, in a team', async () => {
+        // Just make sure creating a player actually works.
+        const player = await createPlayer(['me', 'other'], 'player');
+        const teamAndSeason = await createSeasonAndTeam(false, false);
+        const playerDocId = player.id;
+        const teamDocId = teamAndSeason.team.id;
+        const seasonDocId = teamAndSeason.season.id;
+
+        // Add the player to the team.
+        await teamAndSeason.season.ref.update('players.player', { added: true, me: true, other: true });
+        await teamAndSeason.season.ref.update('users', {
+            me: { added: true, player: true },
+            other: { added: true, player: true },
+        });
+        await teamAndSeason.team.ref.update('players.player', { added: true, me: true, other: true });
+        await teamAndSeason.team.ref.update('users', {
+            me: { added: true, player: true },
+            other: { added: true, player: true },
+        });
+
+        const newPlayerData = player.data()!;
+        newPlayerData['users'] = {
+            me: {
+                added: true,
+                relationship: 'Parent',
+            },
+        };
+
+        const newPlayer = test.firestore.makeDocumentSnapshot(newPlayerData, 'Players/' + playerDocId);
+        await test.wrap(onPlayerWrite)(test.makeChange(player, newPlayer), {
+            auth: {
+                uid: 'me',
+            },
+            authType: 'USER',
+        });
+
+        const data = player;
+        expect(data).to.not.be.null;
+        if (data !== null && data !== undefined) {
+            expect(data.exists).to.be.true;
+            const myData = data.data();
+            expect(myData).to.not.be.null;
+            if (myData !== undefined && myData !== null) {
+                expect(myData.uid).to.equal(playerDocId);
+            }
+        }
+        const seasonData = await admin.firestore().collection('Seasons').doc(seasonDocId).get();
+        if (seasonData !== null && seasonData !== undefined) {
+            expect(seasonData.exists).to.be.true;
+            const myData = seasonData.data();
+            expect(myData).to.not.be.null;
+            if (myData !== undefined && myData !== null) {
+                expect(myData.users).to.deep.equal({
+                    me: {
+                        added: true,
+                        player: true,
+                    },
+
+                });
+                expect(myData.players).to.deep.equal({
+                    player: {
+                        added: true,
+                        me: true,
+                    },
+                });
+            }
+        }
+        const teamData = await admin.firestore().collection('Teams').doc(teamDocId).get();
+        if (teamData !== null && teamData !== undefined) {
+            expect(teamData.exists).to.be.true;
+            const myData = teamData.data();
+            expect(myData).to.not.be.null;
+            if (myData !== undefined && myData !== null) {
+                expect(myData.users).to.deep.equal({
+                    me: {
+                        added: true,
+                        player: true,
+                    },
+
                 });
             }
         }
--- firebase/functions/ts/db/player/update.f.ts
+++ firebase/functions/ts/db/player/update.f.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { DocumentSnapshot } from '@google-cloud/firestore';
 
 const db = admin.firestore();
 const FieldValue = admin.firestore.FieldValue;
@@ -12,11 +13,11 @@ export const onPlayerWrite = functions.firestore.document('/Players/{playerid}')
     const previousData = inputData.before.data();
 
     if (!data) {
-              return data;
+        return data;
     }
     // If we have a new user, update this everywhere.
     for (const user in data.users) {
-          if (!previousData || !(user in previousData.users)) {
+        if (!previousData || !(user in previousData.users)) {
             await addUser(user, inputData.after.id);
         }
     }
@@ -31,6 +32,7 @@ export const onPlayerWrite = functions.firestore.document('/Players/{playerid}')
     return;
 });
 
+// Add this user to the team/seasons.
 async function addUser(user: string, playerId: string): Promise<any> {
     // Add this user everywhere.
     const snapshot = await db
@@ -44,12 +46,41 @@ async function addUser(user: string, playerId: string): Promise<any> {
         updateData['users.' + user + '.' + playerId] = true;
         // Keep the player set correct as well.
         updateData['players.' + playerId + '.' + user] = true;
+
         await db.collection('Seasons').doc(doc.id).update(updateData);
         await db.collection('Teams').doc(doc.data().teamUid).update(updateData);
     }
     return;
 }
 
+// CLeanup the user section if a user is completely removed.
+function fixEmptyUserPlayer(
+    doc: DocumentSnapshot,
+    updateData: Record<string, any>,
+    user: string,
+    playerId: string,
+): void {
+    const docData = doc.data();
+    if (docData && docData.users) {
+        const users = { ...docData.users };
+        if (users) {
+            const checkUser = users[user];
+            if (checkUser) {
+                delete checkUser[playerId];
+                delete checkUser['added'];
+                if (Object.keys(checkUser).length === 0) {
+                    // Remove the user from the team altogether.
+                    updateData['users.' + user] = admin.firestore.FieldValue.delete();
+                    delete updateData['users.' + user + '.' + playerId];
+                       }
+            }
+        }
+
+    }
+    return;
+}
+
+// Remove this specific user from the user/seasons.
 async function removeUser(user: string, playerId: string): Promise<any> {
     // Delete this user everywhere.
     const snapshot = await db
@@ -62,7 +93,19 @@ async function removeUser(user: string, playerId: string): Promise<any> {
         const updateData: Record<string, any> = {};
         updateData['users.' + user + '.' + playerId] = FieldValue.delete();
         updateData['players.' + playerId + '.' + user] = FieldValue.delete();
-        await db.collection('Seasons').doc(doc.id).update(updateData);
+
+        const updateDataSeason: Record<string, any> = { ...updateData };
+        // Check and see if we need to remove the user altogether from the seasons/teams.
+        const teamDoc = await db.collection('Teams').doc(doc.data().teamUid).get();
+
+        fixEmptyUserPlayer(teamDoc, updateData, user, playerId);
+
+        const seasonDoc = await db.collection('Seasons').doc(doc.data().uid).get();
+        fixEmptyUserPlayer(seasonDoc, updateDataSeason, user, playerId);
+
+console.log(updateDataSeason);
+console.log(updateData);
+        await db.collection('Seasons').doc(doc.id).update(updateDataSeason);
         await db.collection('Teams').doc(doc.data().teamUid).update(updateData);
     }
     return;
