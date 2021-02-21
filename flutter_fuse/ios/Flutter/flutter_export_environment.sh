c3db2fa97dfe5e8550e73bd12afe882f3a7431b9
GitHub Action <action@github.com>
2021-02-21 18:39:02 +0000

    Release notes for the ios 0.9.5 version

--- flutter_fuse/ios/Flutter/flutter_export_environment.sh
+++ flutter_fuse/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/hostedtoolcache/flutter/1.26.0-17.6.pre-beta/x64"
-export "FLUTTER_APPLICATION_PATH=/home/runner/work/teamsfuse/teamsfuse/flutter_fuse"
+export "FLUTTER_ROOT=/Users/runner/hostedtoolcache/flutter/1.26.0-17.6.pre-beta/x64"
+export "FLUTTER_APPLICATION_PATH=/Users/runner/work/teamsfuse/teamsfuse/flutter_fuse"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_BUILD_NAME=0.9.4"
-export "FLUTTER_BUILD_NUMBER=0.9.4"
+export "FLUTTER_BUILD_NAME=0.9.5"
+export "FLUTTER_BUILD_NUMBER=0.9.5"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=/Users/runner/work/teamsfuse/teamsfuse/flutter_fuse/.dart_tool/package_config.json"
