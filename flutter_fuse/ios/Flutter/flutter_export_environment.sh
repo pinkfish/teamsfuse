03c4e4d607b0d149097edd54ab74216f55209fe7
GitHub Action <action@github.com>
2021-03-19 21:33:27 +0000

    Update localization for refs/heads/main version

--- flutter_fuse/ios/Flutter/flutter_export_environment.sh
+++ flutter_fuse/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/pinkfishfrog/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/pinkfishfrog/projects/teamsfuse/flutter_fuse"
+export "FLUTTER_ROOT=/opt/hostedtoolcache/flutter/2.0.3-stable/x64"
+export "FLUTTER_APPLICATION_PATH=/home/runner/work/teamsfuse/teamsfuse/flutter_fuse"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
