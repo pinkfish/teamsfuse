d2e739ee07b3bffa31ea04c18ea0e04eeaa76699
David Bennett <pinkfishfrog@gmail.com>
2021-04-03 04:02:18 +0000

    Remove moment from notififyondelete

--- firebase/functions/ts/db/game/notifyoncreate.f.ts
+++ firebase/functions/ts/db/game/notifyoncreate.f.ts
@@ -1,18 +1,18 @@
 import * as functions from 'firebase-functions';
-import * as moment from 'moment';
 import { notifyPayload } from './gamenotifypayload';
 import { PayloadData } from '../../util/notifyforgame';
+import { DateTime } from 'luxon';
 
 export const notifyOnCreate = functions.firestore.document('/Games/{gameid}').onCreate((snap, context) => {
     const data = snap.data();
 
     // Only notify if less then 7 days before the event.
-    const arrivalTime = moment.utc(data.arrivalTime);
-    const nowTime = moment.utc();
+    const arrivalTime = DateTime.fromMillis(data.arrivalTime);
+    const nowTime = DateTime.now().toUTC();
     const diff = arrivalTime.diff(nowTime, 'days');
     let payload: PayloadData | null = null;
     console.log('on create ' + snap.id + ' diff ' + diff);
-    if (diff <= 7 && nowTime.valueOf() < data.time) {
+    if (diff.days <= 7 && nowTime.valueOf() < data.time) {
         console.log('Changed in here');
         // Notify the user of the new event/training/game.
         if (data.type === 'EventType.Practice') {
