b3511c5a240d1623fe99704e39ed5ecb917ec5a6
David Bennett <pinkfishfrog@gmail.com>
2021-04-28 03:07:13 +0000

    Fix up the public_ux dependencies

--- flutter_fuse/ios/Runner/AppDelegate.m
+++ flutter_fuse/ios/Runner/AppDelegate.m
@@ -8,6 +8,9 @@
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   [GMSServices provideAPIKey:@"AIzaSyCzEwmwARRvWz7MGPv9mWfmUQvUPe7hvCM"];
+  if (@available(iOS 10.0, *)) {
+    [UNUserNotificationCenter currentNotificationCenter].delegate = (id<UNUserNotificationCenterDelegate>) self;
+  }
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
